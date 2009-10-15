9d972f235f8d80ca88a98fb893ce9a10228cda96
Bob Remeika <bob.remeika@gmail.com>
2009-10-15 14:45:07 -0700

    Fleshed out a little bit more of the Updater

--- rails/decorators/Updater.js
+++ rails/decorators/Updater.js
@@ -8,16 +8,49 @@ dojo.declare("dojox.rails.decorators.Updater", dojox.rails.decorators.Request, {
   _updaterArgs: {},
 
   constructor: function(domNode){
+    var attributes = this._parseAttributes(this.domNode);
+    var mappedArgs = this._mapAttributes(attributes, dojox.rails.decorators._UpdaterArgMap);
+
+    dojo.mixin(this._updaterArgs, mappedArgs);
+    dojo.connect(this, "onSuccess", this, "_handleSuccess");
+    dojo.connect(this, "onFailure", this, "_handleFailure");
+    dojo.connect(this, "onComplete", this, "_handleComplete");
   },
 
-  parseUpdaterArgs: function(){
-    var drd = dojox.rails.decorators;
-    var keys = drd._UpdaterArgMap.getMappingKeys();
-  }
+  _parseAttributes: function(node){
+    var attrs = node.attributes;
+    var parsedAttrs = {};
+    for (var i = 0; i < attrs.length; i++) {
+      if (!attrs[i]) continue;
 
-//  _parseUpdateHandlers: function(){},
-//  _parsePosition: function(){},
-//  _parseUpdaterArgs: function() {}
+      var matches = attrs[i].name.match(/^data-(.*)/);
+      if (matches && matches.length > 1) {
+        parsedAttrs[matches[1]] = dojo.attr(node, attrs[i].name);
+      }
+    }
+    return parsedAttrs;
+  },
+
+  _mapAttributes: function(attributes, mapper){
+    return mapper.map(attributes);
+  },
+  
+  _handleSuccess: function(request, ioArgs){
+    this._handle(request, ioArgs, this._updaterArgs.successQuery);
+  },
+
+  _handleFailure: function(request, ioArgs){
+    this._handle(request, ioArgs, this._updaterArgs.failureQuery);
+  },
+
+  _handleComplete: function(request, ioArgs){
+    this._handle(request, ioArgs, this._updaterArgs.completeQuery);
+  },
+
+  _handle: function(request, ioArgs, query){
+    var nl = dojo.query(query);
+    nl.place(request.responseText, this._updaterArgs.place);
+  }
 });
 
 
