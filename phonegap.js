a122f12906eed83c1188241bd847c1d43af037dd
foobarfighter <bob.remeika@gmail.com>
2010-05-01 19:15:41 -0700

    Things are in a major state of flux now that I am using dojox.mobile.
    I am now rethinking the entire dojox.phonegap approach. This is one step in that direction where I am no longer relying on phonegap for building ui controls like the TabBar or the ToolBar.  I think dojox.phonegap should only worry about providing a debug console for the browser that lets you simulate hardware events.  All ui controls can be implemented by dojox.mobile and dojox.mobile apps should never need to use the phonegap ui elements.  I'm not totally sure if this is correct at the moment so the code will most likely be changing a lot.

--- phonegap.js
+++ phonegap.js
@@ -1,2 +1,13 @@
 dojo.provide("dojox.phonegap");
-dojo.require("dojox.phonegap.loader");
+// dojo.require("dojox.phonegap.loader");
+
+dojo.require("dojox.mobile.parser");
+
+// Tell the loader that we actually got the dojo.parser module
+// so everything that relies on dojo.parser (e.g. dijit._Templated)
+// thinks the parser is already here.
+dojo.parser = dojox.mobile.parser;
+dojo._loadedModules["dojo.parser"] = dojox.mobile.parser;
+
+dojo.require("dojox.mobile");
+dojo.requireIf(!dojo.isWebKit, "dojox.mobile.compat");
---
+++ phonegap/ui/Toolbar.js
@@ -0,0 +1,62 @@
+dojo.provide("dojox.phonegap.Toolbar");
+dojo.require("dojox.phonegap._Widget");
+
+dojo.declare("dojox.phonegap.Toolbar", [dojox.phonegap._Widget], {
+	_buttons: [],
+	_deviceReadyDispatched: false,
+	show: [],
+	
+	buildRendering: function(){
+		this.inherited(arguments);
+		dojo.query("div", this.domNode).forEach(function(node) {
+			this._addButton(node);
+		}, this);
+		this._removeWidgetNodes();
+	},
+	
+	add: function(button) {
+		this._buttons.push(button);
+	},
+	
+	onDeviceReady: function() {
+		this.ui.createToolBar();
+	},
+	
+	postDeviceReady: function() {
+		this.ui.showToolBarItems.apply(this.ui, this.show);
+	},
+	
+	_addButton: function(node) {
+		var button = new dojox.phonegap.ToolbarButton({
+			title:      dojo.attr(node, "title"),
+			name:       dojo.attr(node, "name"),
+			appearance: dojo.attr(node, "appearance"),
+			topic:      dojo.attr(node, "topic")
+		});
+		this.add(button);
+	},
+	
+	_removeWidgetNodes: function() {
+		dojo.empty(this.domNode);
+		this.domNode.parentNode.removeChild(this.domNode);
+	}
+});
+
+dojo.declare("dojox.phonegap.ToolbarButton", [dojox.phonegap._Widget], {
+	name:       "",
+	title:      "",
+	appearance: "plain",
+	topic:      "",
+	
+	onDeviceReady: function() {
+		var options = { onClick: dojo.hitch(this, "_onClick") };
+		this.ui.createToolBarItem(this.name, this.title, this.appearance, options);
+	},
+	
+	onClick: function(button) {},
+	
+	_onClick: function() {
+		if (this.topic){ dojo.publish(this.topic, [this]); }
+		this.onClick(this);
+	},
+});
